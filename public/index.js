c398ea2e3267b6117ec5e08f22d146c6e6880b6a
N. Martin Abregu <nmabregu@gmail.com>
2022-01-13 16:51:25 -0300

    add delete and edit task

--- public/index.js
+++ public/index.js
@@ -1,6 +1,10 @@
-import { saveTask, getTasks, onTasks } from './services/firebase.js';
+import { saveTask, getTasks, onTasks, deleteTask, getTask, updateTask } from './services/firebase.js';
 
 const taskContainer = document.querySelector('#task-container');
+const taskInput = document.querySelector('#task');
+const taskDescription = document.querySelector('#task-description');
+let editing = false;
+let currentTaskId = '';
 
 window.addEventListener('DOMContentLoaded', async () => {
     onTasks(async (snapshot) => {
@@ -13,14 +17,45 @@ window.addEventListener('DOMContentLoaded', async () => {
 
 const taskForm = document.querySelector('#task-form');
 
+// Click event for the save task button
 taskForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    const taskInput = document.querySelector('#task');
-    const taskDescription = document.querySelector('#task-description');
     
-    saveTask(taskInput.value, taskDescription.value);
+    if (editing) {
+        let data = {
+            task: taskInput.value,
+            description: taskDescription.value
+        };
+        
+        updateTask(currentTaskId, data);
+        
+        editing = false;
+        currentTaskId = '';
+    } else {
+        saveTask(taskInput.value, taskDescription.value);
+    }
     
     taskForm.reset();
+});
+
+// Click event for the delete and update task button
+taskContainer.addEventListener('click', async (e) => {
+    if (e.target.classList.contains('delete-task')) {
+        const id = e.target.dataset.id;
+        if (confirm('Are you sure you want to delete this task?')) await deleteTask(id);
+    }
+    
+    if (e.target.classList.contains('edit-task')) {
+        const id = e.target.dataset.id;
+        const task = await getTask(id);
+        
+        taskInput.value = task.data().task;
+        taskDescription.value = task.data().description;
+        
+        editing = true;
+        currentTaskId = id;
+        document.querySelector('#btn-save-task').innerText = 'Update task';
+    }
 });
 
 async function renderTasks() {
@@ -35,6 +70,8 @@ async function renderTasks() {
                 <h3>${task.task}</h3>
                 <p>${task.description}</p>
                 <small><i>${task.createdAt.toDate()}</i></small>
+                <button class="delete-task" data-id="${doc.id}">Delete</button>
+                <button class="edit-task" data-id="${doc.id}">Edit</button>
             </div>
         `;
     });
--- public/services/firebase.js
+++ public/services/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.2/firebase-app.js";
-import { getFirestore, collection, addDoc, getDocs, onSnapshot } from "https://www.gstatic.com/firebasejs/9.6.2/firebase-firestore.js"
+import { getFirestore, collection, addDoc, getDocs, getDoc, onSnapshot, updateDoc, deleteDoc, doc } from "https://www.gstatic.com/firebasejs/9.6.2/firebase-firestore.js"
 import config from '../config.js';
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -28,6 +28,18 @@ export const getTasks = () => {
     return getDocs(collection(db, 'tasks'));
 }
 
+export const getTask = (id) => {
+    return getDoc(doc(db, 'tasks', id));
+}
+
 export const onTasks = (cb) => {
     onSnapshot(collection(db, 'tasks'), cb);
+}
+
+export const updateTask = (id, data) => {
+    updateDoc(doc(db, 'tasks', id), data);
+}
+
+export const deleteTask = (id) => {
+    deleteDoc(doc(db, 'tasks', id));
 }
