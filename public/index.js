e8015aa4c536606c33919267fb656c95d044a02f
N. Martin Abregu <nmabregu@gmail.com>
2022-01-14 15:09:24 -0300

    add styles tailwind

--- public/index.js
+++ public/index.js
@@ -3,6 +3,8 @@ import { saveTask, getTasks, onTasks, deleteTask, getTask, updateTask } from './
 const taskContainer = document.querySelector('#task-container');
 const taskInput = document.querySelector('#task');
 const taskDescription = document.querySelector('#task-description');
+const btnSaveTask = document.querySelector('#btn-save-task');
+
 let editing = false;
 let currentTaskId = '';
 
@@ -31,6 +33,8 @@ taskForm.addEventListener('submit', (e) => {
         
         editing = false;
         currentTaskId = '';
+        
+        btnSaveTask.innerText = 'Create task';
     } else {
         saveTask(taskInput.value, taskDescription.value);
     }
@@ -54,7 +58,7 @@ taskContainer.addEventListener('click', async (e) => {
         
         editing = true;
         currentTaskId = id;
-        document.querySelector('#btn-save-task').innerText = 'Update task';
+        btnSaveTask.innerText = 'Update task';
     }
 });
 
@@ -66,12 +70,32 @@ async function renderTasks() {
         let task = doc.data();
         
         html += `
-            <div class="task">
-                <h3>${task.task}</h3>
-                <p>${task.description}</p>
-                <small><i>${task.createdAt.toDate()}</i></small>
-                <button class="delete-task" data-id="${doc.id}">Delete</button>
-                <button class="edit-task" data-id="${doc.id}">Edit</button>
+            <div class="task px-3 py-3">
+                <blockquote class="flex items-center justify-between w-full col-span-1 p-6 bg-white rounded-lg shadow">
+                    <div class="flex flex-col pr-8">
+                        <h2 class="text-2xl font-extrabold leading-none text-black sm:text-3xl md:text-5xl">
+                            ${task.task}
+                        </h2>
+                        <div class="relative pl-12">
+                            <p class="mt-2 text-sm text-gray-600 sm:text-base lg:text-sm xl:text-base">
+                                ${task.description}
+                            </p>
+                        </div>
+                        <p class="w-full mt-4 text-sm text-center text-gray-500">
+                            ${task.createdAt.toDate()}
+                        </p>
+                    </div>
+                    <button data-id="${doc.id}" class="delete-task bg-transparent hover:bg-green-500 text-green-700 font-semibold hover:text-white py-3 px-4 border border-green-500 hover:border-transparent rounded">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                        </svg>
+                    </button>
+                    <button data-id="${doc.id}" class="edit-task bg-transparent hover:bg-yellow-500 text-yellow-700 font-semibold hover:text-white py-3 px-4 border border-yellow-500 hover:border-transparent rounded">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+                        </svg>
+                    </button>
+                </blockquote>
             </div>
         `;
     });
